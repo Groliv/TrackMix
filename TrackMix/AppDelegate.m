e1b9b81205e45236b0405062e768e4c6c3661c8f
Olivier Anno <olivier@MacBook.local>
2013-08-25 14:51:56 +0200

    Initial Commit

---
+++ TrackMix/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TrackMix
+//
+//  Created by Olivier Anno on 25/08/13.
+//  Copyright (c) 2013 Olivier Anno. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ TrackMix/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TrackMix
+//
+//  Created by Olivier Anno on 25/08/13.
+//  Copyright (c) 2013 Olivier Anno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
